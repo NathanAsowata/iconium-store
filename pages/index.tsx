8ed2714875a4147bc70ef8d41416f63bd1ef54d6
Nathan Asowata <nathanasowata@gmail.com>
2022-08-15 11:16:06 +0100

    Set up app structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ const Home = () => {
     <>
       <Head>
         <title>Iconium Store</title>
+        <meta name="description" content="Iconium Store is an ecommerce website that offers a variety of
+         products ranging from men's clothing and women's clothing to jewelry and electronics" />
+         <link rel="icon" href="/logo.ico" />
       </Head>
       <h1>Welcome to Iconium store</h1>
     </>
